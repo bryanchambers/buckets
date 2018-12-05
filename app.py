3855d4925e1376dd9c3f50152f3fd3fce3e8fbdc
Bryan <bryches@gmail.com>
2018-12-04 21:38:27 -0500

    Multicolored buckets

--- app.py
+++ app.py
@@ -49,7 +49,8 @@ def home():
             'refill':  bucket.refill,
             'size':    bucket.size,
             'outlook': bucket.balance + bucket.refill,
-            'width':   int((bucket.balance / bucket.size) * 100) if bucket.balance > 0 else 0
+            'width':   int((bucket.balance / bucket.size) * 100) if bucket.balance > 0 else 0,
+            'hue':     240
         })
 
     return render_template('home.html', buckets=buckets, title='Buckets')
